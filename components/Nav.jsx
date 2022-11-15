34fa491f250f27b8a276273f1db197e5476329f9
Cristian-M-B <crisbaronetto@hotmail.com>
2022-11-15 15:09:29 -0300

    add link to home

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,14 @@
+import Link from 'next/link'
 import { Navbar, Text } from '@nextui-org/react'
 
 export default function Nav() {
     return (
         <Navbar isBordered variant='static' css={{ backgroundColor: '#000080', marginBottom: '20px' }}>
-            <Navbar.Brand>
-                <Text b>Tienda Azul</Text>
-            </Navbar.Brand>
+            <Link href='/'>
+                <Navbar.Brand>
+                    <Text b>Tienda Azul</Text>
+                </Navbar.Brand>
+            </Link>
         </Navbar>
     )
 }
