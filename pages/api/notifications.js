3be09d7385b1958fba37a72bd1ba171a9049ed98
Cristian-M-B <crisbaronetto@hotmail.com>
2022-11-15 14:22:23 -0300

    modification

--- pages/api/notifications.js
+++ pages/api/notifications.js
@@ -2,14 +2,11 @@ let notifications = ['0001']
 
 export default function handler(req, res) {
     if (req.method === 'POST') {
-        if (req.body.action === 'payment.created') {
-            console.log(req.body.data.id)
-            notifications.push(req.body)
-            res.status(200).send('OK')
-        }
+        notifications.push(req.body)
+        res.status(200).send('OK')
     }
 
-    if (req.method === 'GET'){
+    if (req.method === 'GET') {
         res.json(notifications)
     }
 }
